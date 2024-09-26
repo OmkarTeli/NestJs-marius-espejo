92dce42452683a2d88b47a2c0bc26fc31dba4f0a
OmkarTeli <omkarteli8055@gmail.com>
2024-09-26 22:50:40 +0530

    feat/add service to the controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,16 +1,33 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
+interface IProduct {
+  id: number;
+  name: string;
+}
 @Controller('product')
 export class ProductController {
+  constructor(private productService: ProductService) {}
+  //   @Get()
+  //   getProducts(): any {
+  //     return [{ id: 0 }];
+  //   }
+  //   @Get(':id')
+  //   getProductById(@Param('id') id: string): { id: number } {
+  //     return {
+  //       id: Number(id),
+  //     };
+  //   }
+
+  //GET ALL PRODUCTS
   @Get()
   getProducts(): any {
-    return [{ id: 0 }];
+    return this.productService.getProducts();
   }
 
+  //GET PRODUCT BY ID
   @Get(':id')
-  getProductsById(@Param('id') id: string): any {
-    return {
-      id,
-    };
+  getProductById(@Param('id') id: number): any {
+    return this.productService.getProductById(id);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 
+interface IProduct {
+  id: number;
+  name: string;
+}
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  private products: IProduct[] = [
+    { id: 1, name: 'iPhone 15' },
+    { id: 2, name: 'iPhone 15 Plus' },
+    { id: 3, name: 'iPhone 15 Pro' },
+    { id: 4, name: 'iPhone 15 Pro MAx' },
+  ];
+
+  async getProducts() {
+    return {
+      success: true,
+      message: 'Products Fetched Successfully',
+      products: this.products,
+    };
+  }
+
+  async getProductById(id: number) {
+    const product = this.products.find((product) => product.id == id);
+    console.log(product);
+    console.log(this.products);
+    return {
+      success: true,
+      message: 'Product  Fetched Successfully',
+      product: product,
+    };
+  }
+}
